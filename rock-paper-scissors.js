6d18e64f6662f26323883908a634d9ea7a3af01e
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-04 15:51:04 -0800

    Building on rock paper scissors
    Trying to refine rock paper scissors game
    Goal: to display previous return values as result on console.log.
    Console.log(compare) does not return the "return" value.
    Current script does not understand "compare == true"... What is the
    value of compare??

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,38 +1,45 @@
-var userChoice = prompt("Do you choose rock, paper, scissors, or rope?");
+var userChoice = prompt("Do you choose rock, paper, or scissors?");
 console.log("Player: " + userChoice);
 
-
-
 var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
+if (computerChoice < .33) {
+  computerChoice = "rock";
+} else if (computerChoice < .67) {
+  computerChoice = "paper";
 } else {
-	computerChoice = "scissors";
+  computerChoice = "scissors";
 } console.log("Computer: " + computerChoice);
 
 var compare = function (userChoice, computerChoice) {
-    if(choice1 === choice2) {
-        userChoice = prompt("It's a tie! Pick again.");
-        console.log("Player: " + userChoice);
-    } else if(choice1 === "rock") {
-        if(choice2 === "scissors") {
-            return "rock wins";
-        } else {
-            return "paper wins";
-        };
-    } else if(choice1 === "paper") {
-        if(choice2 === "rock") {
-            return "paper wins";
-        } else {
-            return "scissors wins";
-        };
-    } else if(choice1 === "scissors") {
-        if(choice2 === "rock") {
-            return "rock wins";
-        } else {
-            return "scissors wins";
-        };
+  if (choice1 === choice2) {
+    return false;
+  } else if (choice1 === "rock") {
+    if (choice2 === "scissors") {
+      return true;
+    } else {
+      return false;
     };
+  } else if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      return true;
+    } else {
+      return false;
+    };
+  } else {
+    if (choice2 === "paper") {
+      return true;
+    } else {
+      return false;
+    };
+  };
+};
+
+console.log(compare);
+
+if (userChoice != ("rock" || "scissors" || "paper")) {
+  console.log("Invalid input. User is a dummy. Computer wins.");
+} else if (compare == true) {
+    console.log("User wins! Great jeorb!!!");
+} else {
+    console.log("Computer wins. Computer grows stronger...");
 };
