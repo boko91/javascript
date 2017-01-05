6213b98a55ee6d68295ed7e1c54526c6c5e89a72
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-05 15:08:55 -0800

    Increased hit points, changed damage per round

--- slay-the-dragon.js
+++ slay-the-dragon.js
@@ -8,15 +8,17 @@ totalDamage = 0;
 
 while (slaying) {
     if (youHit = true) {
-        console.log("You hit the dragon!");
         totalDamage += damageThisRound;
-        if (totalDamage >= 4) {
+        console.log("You hit the dragon for " + damageThisRound + " damage. It roars in rage.");
+        if (totalDamage >= 20) {
+            console.log("The dragon has taken fatal damage. It lets out a smoky final breath. It collapses.");
+            console.log("You have slain the dragon, mighty hero!")
             slaying = false;
         } else {
             youHit = Math.floor(Math.random() * 2);
+            damageThisRound = Math.floor(Math.random()*5 + 1);
         };
     } else {
         console.log("You missed the dragon, and stabbed yourself in the torso. You are dead.");
     };
-    slaying = false;
 };
