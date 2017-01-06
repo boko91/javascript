d7fe88e1b01b45b15ed29e68774f78088ee52fdc
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-05 16:46:39 -0800

    Classic Duck-Duck-Goose game

---
+++ duck-duck-goose.js
@@ -0,0 +1,10 @@
+// Write your loop below!
+
+var duck = Math.floor(Math.random()*2);
+
+do {
+    console.log("Duck...");
+    duck = Math.floor(Math.random()*2);
+} while (duck);
+console.log("GOOSE!");
+    
