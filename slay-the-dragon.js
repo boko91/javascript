1ae28f1367014b18485906e0555dec8e1de3fcac
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-05 16:28:53 -0800

    Include body parts in attack descriptor

--- slay-the-dragon.js
+++ slay-the-dragon.js
@@ -1,5 +1,7 @@
 //plug into http://labs.codecademy.com/#:workspace to test
 
+console.log("Mighty adventurer: Slay the Dragon!");
+
 var slaying = true;
 
 var youHit = Math.floor(Math.random() * 2);
@@ -10,10 +12,15 @@ var totalDamage = 0;
 
 var playerDamage = 0;
 
+var bodyParts = ["head", "chest", "pinky toe", "spleen", "buttocks", "tail", "liver", "funny bone"]
+
+var bodySelect = Math.floor(Math.random() * bodyParts.length);
+
 while (slaying) {
     if (youHit) {
         totalDamage += damageThisRound;
-        console.log("You hit the dragon for " + damageThisRound + " damage. It roars in rage.");
+        bodySelect = Math.floor(Math.random() * bodyParts.length);
+        console.log("You hit the dragon in the " + bodyParts[bodySelect] + " for " + damageThisRound + " damage. It roars in rage.");
         if (totalDamage >= 20) {
             console.log("The dragon has taken fatal damage. It lets out a smoky final breath. It collapses.");
             console.log("You have slain the dragon, mighty hero!")
@@ -21,18 +28,17 @@ while (slaying) {
         } else {
             youHit = Math.floor(Math.random() * 2);
             damageThisRound = Math.floor(Math.random()*5 + 1);
-            console.log(youHit);
         };
     } else {
-        console.log("You missed the dragon, and stabbed yourself for " + damageThisRound + " damage.");
         playerDamage += damageThisRound;
+        bodySelect = Math.floor(Math.random() * bodyParts.length);
+        console.log("You missed the dragon. The dragon slashes you in the " + bodyParts[bodySelect] + " for " + damageThisRound + " damage.");
         if (playerDamage >= 20) {
-            console.log("You have stabbed yourself too many times. You are dead.")
+            console.log("You have lost too many limbs. You are dead.")
             slaying = false;
         } else {
             youHit = Math.floor(Math.random() * 2);
             damageThisRound = Math.floor(Math.random()*5 + 1);
-            console.log(youHit);
         };
     };
 };
