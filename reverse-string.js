0622c78a2ac64ba142c4112cdea4b36ffd428aaa
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-20 14:35:58 -0800

    Reverse String exercise
    reverses string input when summoned with function; also prompts user for
    custom input. Includes white spaces.

---
+++ reverse-string.js
@@ -0,0 +1,16 @@
+var reverseString = function(stringInput) {
+  if (typeof stringInput != "string"){
+    return "Invalid Input";
+  } else {
+    var stringArray = stringInput.split("");
+    var reverseArray = [];
+    for (var i = stringArray.length; i > 0; i--) {
+      reverseArray.push(stringArray[i-1]);
+    };
+    return reverseArray.join("");
+  };
+};
+
+reverseString("Button");
+reverseString("Bourbon Street");
+reverseString(prompt("What would you like reversed?"));
