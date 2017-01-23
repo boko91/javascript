bd04c86c58748b604125662aa3b4b3347f0ebd74
Bonnie Kim <bonniekim@berkeley.edu>
2017-01-23 11:51:06 -0800

    Additional requirements and console log checks

--- pig-latin.js
+++ pig-latin.js
@@ -8,9 +8,14 @@
         remove first letter, print remaining word, add hyphen, add first letter, add "ay"
   ii) Compound consonants - "cheer" becomes "eer-chay"
         take multiple first letters (pre-vowel), print remaining word, add hyphen, add first letters, add "ay"
-  iii) Vowel - "apple" becomes "apple-ay"
+  iii) Vowel - "apple" becomes "apple-way"
         print word, add hyphen, add "way"
-6) Punctuations must still conclude sentences.
+    iv) Compound sounds - "Quaint" becomes "aint-Quay"
+        take multiple first letters, print remaining word, add hyphen, add first letters, add "ay"
+6) Proper capitalization
+    If a given word is capitalized, caps should transfer to beginning of pig latin word.
+7) Punctuations must still conclude sentences.
+    i) define punctuation (".", ",", "?", "!", "'", '"')
 */
 
 var vowelCheck = function (x) {
@@ -19,19 +24,32 @@ var vowelCheck = function (x) {
     return result;
 };
 
+
+
 var pigLatin = function(input) {
   var pigArray = input.split(" ");
-  var newPigArray = [];
+  //console.log(pigArray);
     for (var i = 0; i < pigArray.length; i++ ) {
         var tempArray = pigArray[i].split("");
-        tempArray.splice(tempArray.length, 0, "-", tempArray[0], "ay" );
-        tempArray.splice(0,1);
-        pigArray[i] = tempArray.join("");
+        //console.log(tempArray);
+        //console.log(vowelCheck(tempArray[0]));
+            if (vowelCheck(tempArray[0]) === true) {
+                tempArray.splice(tempArray.length, 0, "-way");
+            } else {
+                var tempChar = [];
+                while (vowelCheck(tempArray[0]) != true) {
+                    tempChar.push(tempArray[0]);
+                    console.log(tempChar);
+                    tempArray.splice(0,1);
+                }
+                tempArray.splice(tempArray.length, 0);
+                tempArray.splice(tempArray.length, 0, tempChar)
+            } pigArray[i] = tempArray.join("");
     }
     pigArray = pigArray.join(" ");
     return pigArray;
 };
 
-pigLatin("Cheery New Year Anne!");
+pigLatin("Screechy New Year Anne");
 
 //pigLatin(prompt("What would you like pig latin-ified?"));
